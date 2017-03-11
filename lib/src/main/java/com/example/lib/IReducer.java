52ce2507af21133a3011e94da85597098bd1805b
kang <kjxstar@gmail.com>
2017-03-11 20:19:14 +0800

    rm state interface

--- lib/src/main/java/com/example/lib/IReducer.java
+++ lib/src/main/java/com/example/lib/IReducer.java
@@ -3,10 +3,10 @@ package com.example.lib;
 /**
  * Created by kang on 17-3-10.
  */
-public interface IReducer<S extends State, A extends Action> {
+public interface IReducer<S, A extends Action> {
 	S reduce(S state, A action);
 
-	class Reducer<S extends State, A extends Action> implements IReducer<S, A> {
+	class Reducer<S, A extends Action> implements IReducer<S, A> {
 		@Override
 		public S reduce(S state, A action) {
 			return state;
--- lib/src/main/java/com/example/lib/State.java
+++
@@ -1,7 +0,0 @@
-package com.example.lib;
-
-/**
- * Created by kang on 17-3-10.
- */
-public enum  State {
-}

17b2ffa6ea46c9526a9c2fa5d1d391c845e8dbea
kang <kjxstar@gmail.com>
2017-03-13 14:41:17 +0800

    add new constructor to TodoContent.java

--- app/src/main/java/com/example/kang/redux/models/TodoContent.java
+++ app/src/main/java/com/example/kang/redux/models/TodoContent.java
@@ -4,6 +4,24 @@ package com.example.kang.redux.models;
  * Created by kang on 17-3-13.
  */
 public class TodoContent {
+    public TodoContent(String content, TodoFilter filter) {
+        this.content = content;
+        this.filter = filter;
+    }
+
+    public TodoContent(String content) {
+        this.content = content;
+        this.filter = TodoFilter.NEW;
+    }
+
+    public TodoContent() {
+    }
+
     public String content;
     public TodoFilter filter;
+
+    @Override
+    public String toString() {
+        return content;
+    }
 }

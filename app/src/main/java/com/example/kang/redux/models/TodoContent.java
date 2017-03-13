65b6ef2ce6c50bbad0b732c6f84c5043a81a205e
kang <kjxstar@gmail.com>
2017-03-13 14:18:59 +0800

    add content model

--- app/src/main/java/com/example/kang/redux/models/TodoContent.java
+++ app/src/main/java/com/example/kang/redux/models/TodoContent.java
@@ -4,4 +4,6 @@ package com.example.kang.redux.models;
  * Created by kang on 17-3-13.
  */
 public class TodoContent {
+    public String content;
+    public TodoFilter filter;
 }
--- app/src/main/java/com/example/kang/redux/models/TodoFilter.java
+++ app/src/main/java/com/example/kang/redux/models/TodoFilter.java
@@ -3,5 +3,8 @@ package com.example.kang.redux.models;
 /**
  * Created by kang on 17-3-13.
  */
-public class TodoFilter {
+public enum TodoFilter {
+    ALL,
+    NEW,
+    OVER
 }

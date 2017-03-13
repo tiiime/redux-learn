2f14a935fd327b208f60cbaf66976b37d48a0e79
kang <kjxstar@gmail.com>
2017-03-13 14:17:36 +0800

    add toString to Action

--- lib/src/main/java/com/example/lib/Action.java
+++ lib/src/main/java/com/example/lib/Action.java
@@ -16,4 +16,9 @@ public class Action<T> {
     public T getContent() {
         return content;
     }
+
+    @Override
+    public String toString() {
+        return String.format("type: %s ---> %s",type,content.toString());
+    }
 }

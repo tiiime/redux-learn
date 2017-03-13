9d6cc5a6d970be684964ef15b3716bfb4d3c4d07
kang <kjxstar@gmail.com>
2017-03-13 13:06:27 +0800

    simple store

--- lib/src/main/java/com/example/lib/Store.java
+++ lib/src/main/java/com/example/lib/Store.java
@@ -1,12 +1,81 @@
 package com.example.lib;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by kang on 17-3-11.
  */
-public interface Store<S> {
-	S getState();
-	void dispatch(Action action);
+public abstract class Store<S> implements IDispatcher {
 
-	void subscribe();
-	void unSubscribe();
+    public abstract S getState();
+
+    public abstract void dispatch(Action action);
+
+    public abstract Subscription subscribe(Subscriber subscriber);
+
+    public abstract void unSubscribe(Subscriber subscriber);
+
+    public static <S> CoreStore<S> create(S currentState, IReducer.Reducer<S> reducer, Middleware<S>... middlewares) {
+        return new CoreStore<S>(currentState, reducer, middlewares);
+    }
+    public static <S> CoreStore<S> create(S currentState, IReducer.Reducer<S> reducer) {
+        return new CoreStore<S>(currentState, reducer, null);
+    }
+
+    private static class CoreStore<S> extends Store<S> {
+        private List<Subscriber> subscribers = new ArrayList<>();
+        private IReducer.Reducer<S> reducer;
+        private S currentState;
+        private IDispatcher mDispatcher = new IDispatcher() {
+            @Override
+            public void dispatch(Action action) {
+                currentState = reducer.reduce(currentState, action);
+
+                for (Subscriber item : subscribers) {
+                    item.onStateUpdate();
+                }
+            }
+        };
+
+        private CoreStore(S currentState, IReducer.Reducer<S> reducer, Middleware<S> ... middlewareArray) {
+            this.reducer = reducer;
+            this.currentState = currentState;
+
+            IDispatcher dispatcher = mDispatcher;
+            for (Middleware item:middlewareArray){
+                dispatcher = item.dispatch(this, dispatcher);
+            }
+            mDispatcher = dispatcher;
+        }
+
+        @Override
+        public S getState() {
+            return currentState;
+        }
+
+        @Override
+        public void dispatch(Action action) {
+            mDispatcher.dispatch(action);
+        }
+
+
+
+        @Override
+        public Subscription subscribe(final Subscriber subscriber) {
+            subscribers.add(subscriber);
+            return new Subscription() {
+                @Override
+                public void unregister() {
+                    subscribers.remove(subscriber);
+                }
+            };
+        }
+
+        @Override
+        public void unSubscribe(Subscriber subscriber) {
+            subscribers.remove(subscriber);
+        }
+    }
+
 }

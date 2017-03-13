209e30ddec484e004dff58e105ef47c3bb032cc3
kang <kjxstar@gmail.com>
2017-03-13 14:18:10 +0800

    add TodoApp state

--- app/src/main/java/com/example/kang/redux/models/TodoState.java
+++ app/src/main/java/com/example/kang/redux/models/TodoState.java
@@ -1,7 +1,17 @@
 package com.example.kang.redux.models;
 
+import java.util.List;
+
 /**
  * Created by kang on 17-3-13.
  */
 public class TodoState {
+
+    public TodoFilter filter =TodoFilter.ALL;
+    public List<TodoContent> content;
+
+    public TodoState(TodoFilter filter, List<TodoContent> content) {
+        this.filter = filter;
+        this.content = content;
+    }
 }

6ab9dc53a0b2fb0c43a96e72884c8c8ae77dc11d
kang <kjxstar@gmail.com>
2017-03-14 13:13:44 +0800

    fix redo middleware

--- app/src/main/java/com/example/kang/redux/redux/middleware/RedoMiddleware.java
+++ app/src/main/java/com/example/kang/redux/redux/middleware/RedoMiddleware.java
@@ -2,13 +2,12 @@ package com.example.kang.redux.redux.middleware;
 
 import com.example.kang.redux.models.TodoState;
 import com.example.kang.redux.redux.Actions;
-import com.example.kang.redux.redux.NotRecordActionCreator;
+import com.example.kang.redux.redux.ResetStateActionCreator;
 import com.example.lib.Action;
 import com.example.lib.IDispatcher;
 import com.example.lib.Middleware;
 import com.example.lib.Store;
 
-import java.util.ArrayList;
 import java.util.Stack;
 
 /**
@@ -18,7 +17,7 @@ public class RedoMiddleware implements Middleware<TodoState> {
     private Stack<TodoState> redoStack = new Stack<>();
     private Stack<TodoState> undoStack = new Stack<>();
 
-    NotRecordActionCreator<TodoState> notRecordActionCreator = new NotRecordActionCreator<>();
+    ResetStateActionCreator<TodoState> resetStateActionCreator = new ResetStateActionCreator<>();
 
     @Override
     public IDispatcher create(final Store<TodoState> store, final IDispatcher nextDispatcher) {
@@ -27,19 +26,28 @@ public class RedoMiddleware implements Middleware<TodoState> {
             public void dispatch(Action action) {
                 switch (action.type) {
                     case Actions.ACTION_UNDO_TODO:
-                        TodoState undoState = undoStack.pop();
-                        redoStack.add(undoState);
-                        store.dispatch(notRecordActionCreator.create(undoState));
+                        try {
+                            TodoState undoState = undoStack.pop();
+                            redoStack.add(new TodoState(store.getState()));
+                            store.dispatch(resetStateActionCreator.create(undoState));
+                        } catch (Exception e) {
+//                            e.printStackTrace();
+                        }
                         break;
                     case Actions.ACTION_REDO_TODO:
-                        TodoState redoState = redoStack.pop();
-                        store.dispatch(notRecordActionCreator.create(redoState));
+                        try {
+                            TodoState redoState = redoStack.pop();
+                            undoStack.add(new TodoState(store.getState()));
+                            store.dispatch(resetStateActionCreator.create(redoState));
+                        } catch (Exception e) {
+//                            e.printStackTrace();
+                        }
                         break;
-                    case Actions.ACTION_NOT_RECORD:
+                    case Actions.ACTION_RESET_STATE:
                         nextDispatcher.dispatch(action);
                         break;
                     default:
-                        undoStack.add(store.getState());
+                        undoStack.add(new TodoState(store.getState()));
                         nextDispatcher.dispatch(action);
                 }
             }
